883da1a33540bcaee4d0770b15a8cdfba23699d7
unknown <kolinszeto@gmail.com>
2025-03-04 21:24:20 -0500

    added in the compeittion homming and lunaching

--- ardiuno code/hdrive_controller_comp/hdrive_controller_comp.ino
+++ ardiuno code/hdrive_controller_comp/hdrive_controller_comp.ino
@@ -1,6 +1,9 @@
 #define LPWM 7  // Motor driver LPWM
 #define RPWM 6  // Motor driver RPWM
 #define servo_p 5  // Motor driver LPWM
+#include <Servo.h>
+
+Servo myServo; // Create a Servo object
 
 void setup() {
     pinMode(RPWM, OUTPUT);
@@ -9,6 +12,8 @@ void setup() {
 
     Serial.begin(9600);  // Initialize Serial Monitor
     Serial.println("Press 'A' to rotate motor one full revolution.");
+    myServo.attach(5); // Attach the servo to pin 5
+
 }
 
 void loop() {
@@ -17,19 +22,40 @@ void loop() {
         
         if (command == 'A' || command == 'a') {  // If 'A' key is pressed
             Serial.println("Motor rotating one full revolution...");
-            
-            encoderTicks = 0;  // Reset encoder count
-            
-            // Start motor forward
-            analogWrite(RPWM, 150);  // Set speed
+                        
+
+
+            // Fire
+            analogWrite(RPWM, 0);  // Set speed
+            analogWrite(LPWM, 255);
+
+            delay(2000/2);        // Wait 1 second
+
+            // Reset motor
+            analogWrite(RPWM, 100);  // Set speed
             analogWrite(LPWM, 0);
+
+            delay(2000);        // Wait 1 second
 
             // Stop the motor
             analogWrite(RPWM, 0);
             analogWrite(LPWM, 0);
             Serial.println("Rotation complete.");
+
+
+            // servo
+/*
+            digitalWrite(servo_p,1800);
+            myServo.write(0);   // Move to 0 degrees
+            delay(1000);        // Wait 1 second
+
+            myServo.write(1800);  // Move to 90 degrees
+            delay(1000);        // Wait 1 second
+            myServo.write(0); // Move to 180 degrees
+            delay(1000);        // Wait 1 second
+
+            Serial.println("servo complete.");
+*/
         }
     }
-}
-
 }
